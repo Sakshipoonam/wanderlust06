669b4afff9bf00697128e47cb03cb21628b2beba
Sakshi <sakshipoonam67@gmail.com>
2025-01-04 13:12:39 +0530

    wanderlust project

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ const ExpressEror = require("../utils/ExpressError.js");
 
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview , isLoggedIn} = require("../middleware.js");
+const {validateReview , isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 
@@ -31,6 +31,7 @@ router.post("/" ,
  //Delete Review Route 
 router.delete("/:reviewId" , 
     isLoggedIn,
+    isReviewAuthor,
     wrapAsync(async(req, res) => {
     let { id, reviewId } = req.params;
  
